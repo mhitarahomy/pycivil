a4f9030332a1092bff73a0d84b3bcc99984234d3
mahdi <mhitarahomy@gmail.com>
2020-04-30 15:41:52 +0430

    Add Snow class to loads

---
+++ loads/snow.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Roof_Condition(Enum):
+    Roof_Condition0 = 0  # هیچ کدام
+    Roof_Condition1 = 1  # برف ریز
+    Roof_Condition2 = 2  # نیمه برف گیر
+    Roof_Condition3 = 3  # برف گیرi
+
+
