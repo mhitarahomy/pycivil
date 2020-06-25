c406827d83c32c2b66c99fca618aaed3571dfe54
mahdi <mhitarahomy@gmail.com>
2020-06-25 12:25:05 +0430

    Add components class

---
+++ structure/building/loads/component.py
@@ -0,0 +1,9 @@
+class part:
+    def __init__(self, name: str, density: float, volume: float):
+        self.name = name
+        self.density = density
+        self.volume = volume
+        self.weight = density * volume
+
+
+
